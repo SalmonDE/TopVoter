d5f4a06208e40758fa881cde4b85592d88b256a2
SalmonDE <salmonde@salmonde.de>
2016-10-15 09:03:00 +0200

    Prevent overriding existing floating texts and add ...
    a suggestion by @Aericio

--- src/SalmonDE/Tasks/QueryServerListTask.php
+++ src/SalmonDE/Tasks/QueryServerListTask.php
@@ -9,18 +9,18 @@ use pocketmine\utils\Utils;
 class QueryServerListTask extends AsyncTask
 {
 
-    public function __construct(Array $data, String $header){
+    public function __construct(Array $data, Array $lines){
         $this->data = $data;
-        $this->header = $header;
+        $this->lines = $lines;
     }
 
     public function onRun(){
         $request = trim(Utils::getURL('https://minecraftpocket-servers.com/api/?object=servers&element=voters&key='.$this->data['Key'].'&month=current&format=json&limit='.$this->data['Amount']));
         if($request != 'Error: server key not found'){
             $information = json_decode($request, true);
-            $text[] = TF::DARK_GREEN.$this->header;
+            $text[] = TF::DARK_GREEN.$this->lines['Header'];
             foreach($information['voters'] as $voter){
-                $text[$voter['nickname']] = TF::GOLD.$voter['nickname'].' '.TF::BLUE.$voter['votes'];
+                $text[$voter['nickname']] = TF::GOLD.str_replace(['{player}', '{votes}'], [$voter['nickname'], $voter['votes']], $this->lines['Text']);
             }
             $text = implode("\n", $text);
             $this->setResult($text);
