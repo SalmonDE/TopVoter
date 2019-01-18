cff5ea936c2fc39540aac9c89393aeb1ebe986e7
SalmonDE <salmonde@salmonde.de>
2019-01-18 16:05:10 +0100

    API 4.0.0: CancellableTrait for events

--- src/SalmonDE/TopVoter/Events/DataUpdateEvent.php
+++ src/SalmonDE/TopVoter/Events/DataUpdateEvent.php
@@ -4,10 +4,12 @@ declare(strict_types = 1);
 namespace SalmonDE\TopVoter\Events;
 
 use pocketmine\event\Cancellable;
+use pocketmine\event\CancellableTrait;
 use pocketmine\event\plugin\PluginEvent;
 use SalmonDE\TopVoter\TopVoter;
 
 class DataUpdateEvent extends PluginEvent implements Cancellable {
+    use CancellableTrait;
 
     private $voteData;
 
